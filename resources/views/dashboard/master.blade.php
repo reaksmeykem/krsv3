71a1e3bc38a1bb849776c64315037e5a66f3c9de
reaksmeykem <79095458+reaksmeykem@users.noreply.github.com>
2024-08-01 10:31:55 +0700

    update

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ env('APP_NAME') }}</title>
     {{-- google font --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Hanuman:wght@100;300;400;700;900&family=Inter:wght@100..900&display=swap" rel="stylesheet">
@@ -34,18 +34,6 @@
 <x-livewire-alert::scripts />
 
 @include('dashboard.plugin.loading')
-
-
-
-
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-<script src="https://cdn.tailwindcss.com"></script>
-<script src="https://cdn.datatables.net/2.1.2/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/2.1.2/js/dataTables.tailwindcss.js"></script>
-<script src="https://cdn.tailwindcss.com"></script>
-<script>
-    new DataTable('#example');
-</script>
 
 
 @livewireScripts
