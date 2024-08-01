d830fed0982a00531f10b04b46250a7eebd17e60
reaksmeykem <79095458+reaksmeykem@users.noreply.github.com>
2024-08-01 16:26:34 +0700

    update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta http-equiv="refresh" content="1">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
