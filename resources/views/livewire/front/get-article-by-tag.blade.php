ae1bee94e4e6565291b42d50216e3fd7d8e0028f
reaksmeykem <79095458+reaksmeykem@users.noreply.github.com>
2024-08-04 20:09:30 +0700

    update

--- resources/views/livewire/front/get-article-by-tag.blade.php
+++ resources/views/livewire/front/get-article-by-tag.blade.php
@@ -3,8 +3,8 @@
         <h3 class="text-4xl font-black leading-[48px] mb-8">{{ $tagName }}</h3>
         <div class="space-y-6">
             @foreach($posts as $article)
-                <div class="block space-y-2 md:flex md:items-center md:space-x-10">
-                    <span class="min-w-[80px]">{{ Carbon\Carbon::create($article->published_at)->format('F d, Y')  }}</span>
+                <div class="block space-y-2 md:flex md:items-center md:space-x-8">
+                    <span class="w-[140px]">{{ Carbon\Carbon::create($article->published_at)->format('F d, Y')  }}</span>
                     <div>
                         <a href="{{ route('post.detail', [$article->category->slug, $article->slug]) }}" wire:navigate><h3 class="text-xl hover:text-[#F4CE14]">{{ $article->title }}</h3></a>
                     </div>
