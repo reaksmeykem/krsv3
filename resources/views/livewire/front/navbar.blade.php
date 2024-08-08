8a969192d7291a38707c7722023ff84eb3a89ec2
reaksmeykem <79095458+reaksmeykem@users.noreply.github.com>
2024-08-08 19:53:11 +0700

    update

--- resources/views/livewire/front/navbar.blade.php
+++ resources/views/livewire/front/navbar.blade.php
@@ -82,7 +82,7 @@
                                         </div>
                                     @endforeach
                                 </div>
-                                <div class="text-center mt-[60px]">
+                                <div class="text-center my-[60px]">
                                     @if($results->hasMorePages())
                                         <span role="status" wire:loading wire:target="loadMore" class="ml-2">
                                             <svg aria-hidden="true" class="inline w-7 h-7 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
